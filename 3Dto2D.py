759ad044eaa47c6e8361b0ef621786176d137523
Edgar Ortiz <ed.ortizm@gmail.com>
2020-03-16 10:23:17 -0300

    now is possible to accept either filter name or wavelenght range (this accounts for 2 arguments). Assertins errors added

--- 3Dto2D.py
+++ 3Dto2D.py
@@ -5,13 +5,21 @@ import numpy as np
 import sys
 # sys.arg[0] is the name of the script itself (careful)
 ################################ code starts ###################################
-file_name = sys.argv[1]
-filter_name= sys.argv[2]
-
 # raising an assetion error when the script is not provided with the right number of n_arguments or the right format
 #https://stackoverflow.com/questions/1964126/what-exception-to-raise-if-wrong-number-of-arguments-passed-in-to-args
 
 n_arguments = len(sys.argv)
-assert n_arguments == 3, "Please provide only two arguments to the script: arg1 = file name, arg2= filter name "
-assert (file_name[-5:] == ".fits"), "Please remember that the format for the file name is fits: file_name.fits, (lower case)"
-assert (filter_name[-5:] == ".fits"), "Please remember that the format for the filter name is fits: fiter_name.fits, (lower case)"
+if n_arguments == 3:
+    file_name = sys.argv[1]
+    assert (file_name[-5:] == ".fits"), "The format of the file is .fits: file_name.fits (lower case)"
+    filter_name= sys.argv[2]
+#assert (filter_name[-5:] == ".fits"), "Please remember that the format for the filter name is fits: fiter_name.fits, (lower case)"
+# or wavelenght range
+elif n_arguments == 4:
+    file_name = sys.argv[1]
+    assert (file_name[-5:] == ".fits"), "The format of the file is .fits: file_name.fits (lower case)"
+    lambda1 = float(sys.argv[2])
+    lambda2 = float(sys.argv[3])
+    assert lambda1 < lambda2, "first wavelenght must be smaller than the second one"
+else:
+    assert False, "# of args must be 2 (file and filter name) OR 3 (file name, lower wavelenght and higher wavelenght)"
